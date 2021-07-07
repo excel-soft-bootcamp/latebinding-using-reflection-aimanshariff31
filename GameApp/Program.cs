5fce05a820baadfbdab6d5e5399bddf9044c3a14
aimanshariff31 <86050998+aimanshariff31@users.noreply.github.com>
2021-07-07 15:58:58 +0530

    Add files via upload

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -10,60 +10,126 @@ namespace GameApp
     //Constant Value is - int
     public enum Options
     {
-        BASIC=1,INTERMEDIATE,ADVANCED
+        BASIC = 1, INTERMEDIATE, ADVANCED
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Word Guess Game");
-            
-            string message = string.Format("Enter Your Choice {0}->Basic , {1}->Intermediate ,{2}->Advanced",Options.BASIC,Options.INTERMEDIATE,Options.ADVANCED);// 1->Basic,2->Intermediate
+
+            string message = string.Format("Enter Your Choice {0}->Basic , {1}->Intermediate ,{2}->Advanced", Options.BASIC, Options.INTERMEDIATE, Options.ADVANCED);// 1->Basic,2->Intermediate
             //String Interpollation 
             string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
             Console.WriteLine(displayMessage);
-            Options _choice =(Options)Int32.Parse( Console.ReadLine());
-            switch (_choice)
+            Options _choice = (Options)Int32.Parse(Console.ReadLine());
+            try
             {
-                case Options.BASIC:
-                    Console.WriteLine("Basic Level");
-                    //Use Reflection  
-                    //Assembly Load
-                    System.Reflection.Assembly basicLevelLib = 
-      System.Reflection.Assembly.LoadFile(@"C:\Users\user\source\repos\excelsoft\Examples\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll");
-                    // Search For Class - BasicLevelType
-                   System.Type basicLevelTypeClassRef= basicLevelLib.GetType("BasicLevelLib.BasicLevelType");
-                    if (basicLevelTypeClassRef != null)
-                    {
-                        if (basicLevelTypeClassRef.IsClass)
+
+
+                switch (_choice)
+                {
+                    case Options.BASIC:
+                        Console.WriteLine("Basic Level");
+                        //Use Reflection  
+                        //Assembly Load
+                        System.Reflection.Assembly basicLevelLib =
+          System.Reflection.Assembly.LoadFile(@"C:\Users\user\source\repos\excelsoft\Examples\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll");
+                        // Search For Class - BasicLevelType
+                        System.Type basicLevelTypeClassRef = basicLevelLib.GetType("BasicLevelLib.BasicLevelType");
+                        if (basicLevelTypeClassRef != null)
                         {
-                            //Instantiate Type
-                            //BasicLevelLib.BasicLevelType objref=new BasicLevelLib.BasicLevelType() ; Early Binding
-                           Object objRef =System.Activator.CreateInstance(basicLevelTypeClassRef); //LateBinding Code
-                            //Discove Method
-                          System.Reflection.MethodInfo _methodRef=  basicLevelTypeClassRef.GetMethod("Play");
-                            if (!_methodRef.IsStatic)
+                            if (basicLevelTypeClassRef.IsClass)
                             {
-                                //Invoke NonStatic Method
-                                // string Play(string playerName, int earlierPoints){}
-                                //object result=  _methodRef.Invoke(objRef, new object[] {"Tom",20 });
-                                object result =  _methodRef.Invoke(objRef, new object[] {});
-                                Console.WriteLine(result.ToString());
+                                //Instantiate Type
+                                //BasicLevelLib.BasicLevelType objref=new BasicLevelLib.BasicLevelType() ; Early Binding
+                                Object objRef = System.Activator.CreateInstance(basicLevelTypeClassRef); //LateBinding Code
+                                                                                                         //Discove Method
+                                System.Reflection.MethodInfo _methodRef = basicLevelTypeClassRef.GetMethod("Play");
+                                if (!_methodRef.IsStatic)
+                                {
+                                    //Invoke NonStatic Method
+                                    // string Play(string playerName, int earlierPoints){}
+                                    //object result=  _methodRef.Invoke(objRef, new object[] {"Tom",20 });
+                                    object result = _methodRef.Invoke(objRef, new object[] { });
+                                    Console.WriteLine(result.ToString());
+                                }
+
                             }
 
                         }
+                        break;
+                    case Options.INTERMEDIATE:
+                        Console.WriteLine("Intermediate Level");
+                        System.Reflection.Assembly intermediateLevelLib =
+                        System.Reflection.Assembly.LoadFile(@"C:\Users\user\source\repos\excelsoft\Examples\GameApp\bin\Debug\LevelLibs\IntermediateLevelLib.dll");
+                        System.Type intermediateTypeClassRef = intermediateLevelLib.GetType("IntermediateLevelLib.IntermediateLevelType");
 
-                    }
-                     break;
-                case Options.INTERMEDIATE:
-                    Console.WriteLine("Intermediate Level");
-                     break;
-                case Options.ADVANCED:
-                    Console.WriteLine("Advanced Level");
-                    break;
-                
+                        if (intermediateTypeClassRef != null)
+                        {
+                            if (intermediateTypeClassRef.IsClass)
+                            {
+
+                                Object objRef = System.Activator.CreateInstance(intermediateTypeClassRef);
+                                System.Reflection.MethodInfo _methodRef = intermediateTypeClassRef.GetMethod("Start");
+                                if (!_methodRef.IsStatic)
+                                {
+                                    object result = _methodRef.Invoke(objRef, new object[] { "Shariff" });
+                                    Console.WriteLine(result.ToString());
+                                }
+
+                            }
+
+                        }
+                        break;
+                    case Options.ADVANCED:
+                        Console.WriteLine("Advanced Level");
+                        System.Reflection.Assembly advancedLevelLib =
+                        System.Reflection.Assembly.LoadFile(@"C:\Users\user\source\repos\excelsoft\Examples\GameApp\bin\Debug\LevelLibs\AdvancedLevelLib.dll");
+                        System.Type advancedTypeClassRef = advancedLevelLib.GetType("AdvancedLevelLib.AdvancedLevelType");
+
+                        if (advancedTypeClassRef != null)
+                        {
+                            if (advancedTypeClassRef.IsClass)
+
+                            {
+
+                                Object objRef = System.Activator.CreateInstance(advancedTypeClassRef);
+                                System.Reflection.MethodInfo _methodRef = advancedTypeClassRef.GetMethod("Begin");
+                                if (!_methodRef.IsStatic)
+                                {
+
+                                    object result = _methodRef.Invoke(objRef, new object[] { "Aiman", 5 });
+                                    Console.WriteLine(result.ToString());
+                                }
+
+                            }
+
+                        }
+                        break;
+                }
+
             }
 
+
+
+            catch (Exception cs)
+            {
+                Console.WriteLine("Select Appropriate Number " + cs);
+            }
+            finally { }
+
+            
         }
     }
+
 }
+
+        
+    
+
+            
+
+        
+    
+
